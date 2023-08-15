9aec01f814efa506ce6cbfb0b4f7ae5fd95296db
Juan C Olmedo <doubleparked88@gmail.com>
2023-08-14 21:15:20 -0700

    method implemented

--- app/src/main/java/com/jco/taskmaster/activities/CreateTask.java
+++ app/src/main/java/com/jco/taskmaster/activities/CreateTask.java
@@ -3,6 +3,8 @@ package com.jco.taskmaster.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jco.taskmaster.R;
 
@@ -12,5 +14,12 @@ public class CreateTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        Button addTaskButton = findViewById(R.id.CreateTaskActivitySubmitButton);
+
+        addTaskButton.setOnClickListener(v -> {
+            TextView myTextView = findViewById(R.id.CreateActivityTaskTitle);
+            myTextView.setText("Submitted");
+        });
     }
 }
