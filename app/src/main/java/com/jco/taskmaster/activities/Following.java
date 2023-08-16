3b5fc8248966a3cf5e621ce01d81e221a0e09f36
Juan C Olmedo <doubleparked88@gmail.com>
2023-08-15 20:40:12 -0700

    need to implement SharedPreferences to be able to save new username and display

---
+++ app/src/main/java/com/jco/taskmaster/activities/Following.java
@@ -0,0 +1,17 @@
+package com.jco.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jco.taskmaster.R;
+
+public class Following extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_following);
+    }
+
+}
